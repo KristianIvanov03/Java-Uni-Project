28cb098538c9820947dcc0176fcb8734e8930c3e
Your Name <kristian.ivanov.pr@gmail.com>
2024-05-20 10:06:47 +0300

    Basic setup for Cashier class

--- src/main/java/org/example/solution/Cashier.java
+++ src/main/java/org/example/solution/Cashier.java
@@ -1,4 +1,25 @@
 package org.example.solution;
 
 public class Cashier {
+    private static int cashierCount = 0;
+    private int id;
+    private String name;
+    private double salary;
+
+    public Cashier(String name, double salary){
+        this.id = generateId();
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public int getId(){
+        return this.id;
+    }
+
+    private synchronized int generateId(){
+        return cashierCount++;
+    }
 }
