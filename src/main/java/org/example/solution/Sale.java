bb9c9b85606f9aea178c9a446f16b22642c1cde7
Your Name <kristian.ivanov.pr@gmail.com>
2024-05-29 19:01:27 +0300

    i think final changes for this class

--- src/main/java/org/example/solution/Sale.java
+++ src/main/java/org/example/solution/Sale.java
@@ -1,4 +1,27 @@
 package org.example.solution;
 
 public class Sale {
+    private Product product;
+    private int quantity;
+
+    public Sale(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return this.product.getName();
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public double getTotalPrice() {
+        return product.calculateSellingPrice() * quantity;
+    }
 }
